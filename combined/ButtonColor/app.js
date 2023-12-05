9dfbe3f0106f05955618468b5279b23733c4fd39
Stuart Murray <stuart.murray.dev@outlook.com>
2023-12-05 12:32:07 +0000

    move to generic handler

--- combined/ButtonColor/app.js
+++ combined/ButtonColor/app.js
@@ -11,7 +11,13 @@ for (const button of buttons) {
   button.addEventListener('click', colorise);
 }
 
+const h1s = document.querySelectorAll('h1');
+
+for (const h1 of h1s) {
+  h1.addEventListener('click', colorise);
+}
+
 function colorise() {
-    this.style.backgroundColor = randomColor();
-    this.style.color = randomColor();
+  this.style.backgroundColor = randomColor();
+  this.style.color = randomColor();
 }
