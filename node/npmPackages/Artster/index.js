2f391966455bbac944fa7fa8a954b06eb4c33fc5
Stuart Murray <stuart.murray.dev@outlook.com>
2023-12-21 10:52:16 +0000

    npm examples

---
+++ node/npmPackages/Artster/index.js
@@ -0,0 +1,10 @@
+const figlet = require('figlet');
+const colors = require('colors');
+
+figlet('Hello World', function (err, data) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(data.green);
+});
