fd4567f8700034271b3dfc208b3ad57ea00b25e4
Stuart Murray <stuart.murray.dev@outlook.com>
2024-01-02 11:20:41 +0000

    add routes

--- node/ExpressIntro/FirstApp/index.js
+++ node/ExpressIntro/FirstApp/index.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res) => {
-  console.log('WE GOT A NEW REQUEST');
-  //   res.send('RESPONSE FROM EXPRESS');
-  //   res.send({ color: 'red' });
-  res.send('<h1>This is a web page</h1>');
+// app.use((req, res) => {
+//   console.log('WE GOT A NEW REQUEST');
+//   //   res.send('RESPONSE FROM EXPRESS');
+//   //   res.send({ color: 'red' });
+//   res.send('<h1>This is a web page</h1>');
+// });
+
+app.get('/', (req, res) => {
+  res.send('This is the home page');
+});
+
+app.get('/cats', (req, res) => {
+  console.log('CAT REQUEST');
+  res.send('MEOW');
+});
+
+app.post('/cats', (req, res) => {
+  console.log('POST CAT REQUEST');
+  res.send('POST REQUEST MEOW');
+});
+
+app.get('/dogs', (req, res) => {
+  console.log('DOG REQUEST');
+  res.send('WOOF');
+});
+
+app.get('*', (req, res) => {
+  res.send(`I don't know that path`);
 });
 
 app.listen(3000, () => {
