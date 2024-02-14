6313a0e32044df70b161aa81813a943b1608ddd7
Stuart Murray <stuart.murray.dev@outlook.com>
2024-02-14 12:34:09 +0000

    more complex data

--- node/ExpressIntro/TemplatingDemo/index.js
+++ node/ExpressIntro/TemplatingDemo/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const redditData = require('./data.json');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
@@ -21,7 +22,8 @@ app.get('/cats', (req, res) => {
 
 app.get('/r/:subreddit', (req, res) => {
   const { subreddit } = req.params;
-  res.render('subreddit', { subreddit });
+  const data = redditData[subreddit];
+  res.render('subreddit', { ...data });
 });
 
 app.listen(3000, () => {
