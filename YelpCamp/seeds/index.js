0e97d3e7b7a2db24c47e000e33502a746e06dbf1
Stuart Murray <stuart.murray.dev@outlook.com>
2024-02-21 16:17:43 +0000

    seed data

---
+++ YelpCamp/seeds/index.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { descriptors, places } = require('./seedHelpers');
+const Campground = require('../models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Database connected');
+});
+
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+const seedDb = async () => {
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const camp = new Campground({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+    });
+    await camp.save();
+  }
+};
+
+seedDb().then(() => mongoose.connection.close());
