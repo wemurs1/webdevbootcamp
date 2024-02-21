5e19a319a60bc5921e721513c61e1f96dfeb6491
Stuart Murray <stuart.murray.dev@outlook.com>
2024-02-21 11:05:31 +0000

    seed data

---
+++ ExpressMongoose/First/seeds.js
@@ -0,0 +1,58 @@
+const Product = require('./models/product');
+const mongoose = require('mongoose');
+
+mongoose
+  .connect('mongodb://localhost:27017/farmStand')
+  .then(() => {
+    console.log('MONGO CONNECTION OPEN');
+  })
+  .catch((err) => {
+    console.log('MONGO CONNECTION ERROR');
+    console.log(err);
+  });
+
+// const p = new Product({
+//   name: 'Ruby Grapefruit',
+//   price: 1.99,
+//   category: 'fruit',
+// });
+
+// p.save()
+//   .then((p) => {
+//     console.log(p);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+const seedProducts = [
+  {
+    name: 'Fairy Eggplant',
+    price: 1.0,
+    category: 'vegetable',
+  },
+  {
+    name: 'Organic Goddess Melon',
+    price: 4.99,
+    category: 'fruit',
+  },
+  {
+    name: 'Organic Mini Seedless Watermelon',
+    price: 3.99,
+    category: 'fruit',
+  },
+  {
+    name: 'Organic Celery',
+    price: 1.5,
+    category: 'vegetable',
+  },
+  {
+    name: 'Chocolate Whole Milk',
+    price: 2.69,
+    category: 'dairy',
+  },
+];
+
+Product.insertMany(seedProducts).then((result) => {
+  console.log(result);
+});
