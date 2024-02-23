5a85ceaf3761cc5229e6132160c45fb290b78afe
Stuart Murray <stuart.murray.dev@outlook.com>
2024-02-23 16:52:19 +0000

    basic Joi

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 const ejsMate = require('ejs-mate');
+const Joi = require('joi');
 
 const catchAsyc = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
@@ -42,8 +43,22 @@ app.get('/campgrounds/new', (req, res) => {
 app.post(
   '/campgrounds',
   catchAsyc(async (req, res, next) => {
-    if (!req.body.campground)
-      throw new ExpressError('Invalid Campground Data', 400);
+    // if (!req.body.campground)
+    //   throw new ExpressError('Invalid Campground Data', 400);
+    const campgroundSchema = Joi.object({
+      campground: Joi.object({
+        title: Joi.string().required(),
+        price: Joi.number().required().min(0),
+        image: Joi.string90.required(),
+        location: Joi.string().required(),
+        description: Joi.string().required(),
+      }).required(),
+    });
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+      const msg = error.details.map((el) => el.message).join(',');
+      throw new ExpressError(msg, 400);
+    }
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
