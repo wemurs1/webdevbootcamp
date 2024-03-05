49fba3dd5add499cfcc593dd9343f7f7c3d97649
Stuart Murray <stuart.murray.dev@outlook.com>
2024-03-05 09:16:02 +0000

    Middleware for delete

--- ExpressMongoose/First/models/farm.js
+++ ExpressMongoose/First/models/farm.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
+const Product = require('./product');
+
 const farmSchema = new Schema({
   name: {
     type: String,
@@ -19,6 +21,12 @@ const farmSchema = new Schema({
       ref: 'Product',
     },
   ],
+});
+
+farmSchema.post('findOneAndDelete', async function (farm) {
+  if (farm.products.length) {
+    await Product.deleteMany({ _id: { $in: farm.products } });
+  }
 });
 
 const Farm = mongoose.model('Farm', farmSchema);
