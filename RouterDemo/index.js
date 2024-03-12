972ea363a4d7abfb21c65365a1f11a11b6e0d107
Stuart Murray <stuart.murray.dev@outlook.com>
2024-03-12 09:31:22 +0000

    embed route on index.js

--- RouterDemo/index.js
+++ RouterDemo/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const shelterRoutes = require('./routes/shelters');
 
-app.use('/', shelterRoutes);
+app.use('/shelters', shelterRoutes);
 
 app.listen(3000, () => {
   console.log('Serving app on localhost:3000');
--- RouterDemo/routes/shelters.js
+++ RouterDemo/routes/shelters.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/shelters', (req, res) => {
+router.get('/', (req, res) => {
   res.send('ALL SHELTERS');
 });
 
-router.post('/shelters', (req, res) => {
+router.post('/', (req, res) => {
   res.send('CREATE A SHELTER');
 });
 
-router.get('/shelters/:id', (req, res) => {
+router.get('/:id', (req, res) => {
   res.send('VIEWING ONE SHELTER');
 });
 
-router.get('/shelters/:id/edit', (req, res) => {
+router.get('/:id/edit', (req, res) => {
   res.send('EDITING ONE SHELTER');
 });
 
