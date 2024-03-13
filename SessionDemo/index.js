52056eda4893065882f40123a46a836dbc03e71e
Stuart Murray <stuart.murray.dev@outlook.com>
2024-03-13 09:58:40 +0000

    session

---
+++ SessionDemo/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const session = require('express-session');
+
+app.use(session({ secret: 'thisisnotagoodsecret' }));
+
+app.get('/viewcount', (req, res) => {
+  if (req.session.count) {
+    req.session.count += 1;
+  } else {
+    req.session.count = 1;
+  }
+  res.send(`you have viewed the page ${req.session.count} times`);
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
