27268832777186cc4117b3e456a80e87dcaf0325
Stuart Murray <stuart.murray.dev@outlook.com>
2024-03-18 11:48:44 +0000

    auth demo

---
+++ AuthDemo/index.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+const User = require('./models/user');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
+
+mongoose
+  .connect('mongodb://localhost:27017/authDemo')
+  .then(() => {
+    console.log('Mongo connection open');
+  })
+  .catch((err) => {
+    console.log('Mongo connection error');
+    console.log(err);
+  });
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
+app.use(session({ secret: 'notagoodsecret' }));
+
+app.get('/', (req, res) => {
+  res.send('this is the home page');
+});
+
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 12);
+  const user = new User({
+    username,
+    password: hashedPassword,
+  });
+  await user.save();
+  req.session.user_id = user._id;
+  res.redirect('/');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  const validPassword = await bcrypt.compare(password, user.password);
+  if (validPassword) {
+    req.session.user_id = user._id;
+    res.redirect('/secret');
+  } else {
+    res.redirect('/login');
+  }
+});
+
+app.post('/logout', (req, res) => {
+  req.session.user_id = null;
+  res.redirect('/login');
+});
+
+app.get('/secret', (req, res) => {
+  if (!req.session.user_id) {
+    return res.redirect('/login');
+  }
+  res.render('secret');
+});
+
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+});
