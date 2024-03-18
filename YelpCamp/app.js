a52237086ffb696f09c43ec2383ee5ff51dae92f
Stuart Murray <stuart.murray.dev@outlook.com>
2024-03-18 15:15:21 +0000

    login and register

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -7,10 +7,14 @@ const morgan = require('morgan');
 const ejsMate = require('ejs-mate');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
 const ExpressError = require('./utils/ExpressError');
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
+const userRoutes = require('./routes/user');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
@@ -41,14 +45,28 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
   res.locals.success = req.flash('success');
   res.locals.error = req.flash('error');
   next();
 });
 
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
+app.get('/fakeUser', async (req, res) => {
+  const user = new User({ email: 'colt@gmail.com', username: 'colt' });
+  const newUser = await User.register(user, 'chicken');
+  res.send(newUser);
+});
+
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
+app.use('/', userRoutes);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
