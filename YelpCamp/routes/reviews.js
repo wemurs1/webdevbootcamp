448f7d9f5a38820cee2e7104ce52b5f51df9be23
Stuart Murray <stuart.murray.dev@outlook.com>
2024-03-20 11:10:11 +0000

    move users logic to controller

--- YelpCamp/routes/reviews.js
+++ YelpCamp/routes/reviews.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 
 const catchAsyc = require('../utils/catchAsync');
-const Campground = require('../models/campground');
-const Review = require('../models/review');
 const reviews = require('../controllers/reviews');
 const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware');
 require('../schemas');
