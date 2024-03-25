0bae9c0cdfcdd5cbd636c775273e1bff65792dcc
Stuart Murray <stuart.murray.dev@outlook.com>
2024-03-25 10:00:13 +0000

    image upload and delete

--- YelpCamp/cloudinary/index.js
+++ YelpCamp/cloudinary/index.js
@@ -11,7 +11,7 @@ const storage = new CloudinaryStorage({
   cloudinary,
   params: {
     folder: 'YelpCamp',
-    allowedFormats: ['jpeg', 'jpg', 'png'],
+    allowedFormats: ['jpeg', 'png', 'jpg'],
   },
 });
 
