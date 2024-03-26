c7d901028528f122894969fb6ad842d29bb81f06
Stuart Murray <stuart.murray.dev@outlook.com>
2024-03-26 12:37:12 +0000

    clust map with campground data

--- YelpCamp/seeds/index.js
+++ YelpCamp/seeds/index.js
@@ -24,7 +24,10 @@ const seedDb = async () => {
       title: `${sample(descriptors)} ${sample(places)}`,
       geometry: {
         type: 'Point',
-        coordinates: [-113.1331, 47.0202],
+        coordinates: [
+          cities[random1000].longitude,
+          cities[random1000].latitude,
+        ],
       },
       images: [
         {
